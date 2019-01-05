05f3760fd9a8ccfefabe6f611835e782e7062182
Andi Péter <andipeti2000@gmail.com>
2019-01-05 16:22:40 +0100

    Created lock screen

--- i3/lock_and_blur.sh
+++ i3/lock_and_blur.sh
@@ -1,17 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # set the icon and a temporary location for the screenshot to be stored
-icon="$HOME/images/lock-icon-light.png"
+icon="lock.png"
 tmpbg='/tmp/screen.png'
 
 # take a screenshot
-scrot "$tmpbg"
+scrot $tmpbg
 
 # blur the screenshot by resizing and scaling back up
-convert "$tmpbg" -filter Gaussian -thumbnail 20% -sample 500% "$tmpbg"
+#convert $tmpbg -filter Gaussian -thumbnail 20% -sample 500% $tmpbg
+convert $tmpbg -filter Gaussian -blur 0x7 $tmpbg
 
 # overlay the icon onto the screenshot
-convert "$tmpbg" "$icon" -gravity center -composite "$tmpbg"
+#convert $tmpbg $icon -gravity center -composite $tmpbg
 
 # lock the screen with the blurred screenshot
-i3lock -i "$tmpbg"
+i3lock -i $tmpbg
+
+rm $tmpbg
